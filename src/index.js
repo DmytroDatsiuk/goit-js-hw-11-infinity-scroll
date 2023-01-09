a4571db4756842610cb5412c3c9f2e568924bd6b
DDimbe <109931951+DmytroDatsiuk@users.noreply.github.com>
2023-01-09 07:32:35 +0200

    a

--- src/index.js
+++ src/index.js
@@ -1,46 +1,74 @@
 import NewsApiService from './new-service';
-// import Notiflix from 'notiflix';
-// const axios = require('axios').default;
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import throttle from 'lodash.throttle';
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
-  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
   gallery: document.querySelector('.gallery'),
 };
 
 const newsApiService = new NewsApiService();
 
 refs.searchForm.addEventListener('submit', onSearch);
-refs.loadMoreBtn.addEventListener('click', onLoadMore);
+window.addEventListener('scroll', throttle(checkPosition, 250));
+window.addEventListener('resize', throttle(checkPosition, 250));
 
 function onSearch(e) {
   e.preventDefault();
-  refs.gallery.innerHTML = '';
-
-  refs.loadMoreBtn.classList.remove('visually-hidden');
+  clearGalleryMarkup();
 
   newsApiService.query = e.currentTarget.elements.searchQuery.value;
+
   newsApiService.resetPage();
+
   newsApiService
     .fetchArticles()
     .then(data => {
-      console.log('data.hits', data.hits)
-      // console.log(data.totalHits)
-      // Notiflix.Notify.
+      if (data.totalHits === 0) {
+        Notiflix.Notify.failure(
+          `Sorry, there are no images matching your search query. Please try again.`
+        );
+        return;
+      }
+
       Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-      return appendPhotoHitsMurkup(data.hits);
+      appendPhotoHitsMurkup(data.hits);
     })
-    .catch(error => {});
+    .catch(error => {
+      console.log(error);
+    });
 }
 
-function onLoadMore(e) {
-  console.log(data.hits)
-  newsApiService.fetchArticles().then(data => appendPhotoHitsMurkup(data.hits));
+function checkPosition() {
+  const height = document.body.offsetHeight;
+  const screenHeight = window.innerHeight;
+
+  const scrolled = window.scrollY;
+
+  const threshold = height - screenHeight / 4;
+
+  const position = scrolled + screenHeight;
+
+  if (position >= threshold) {
+    newsApiService
+      .fetchArticles()
+      .then(data => {
+        appendPhotoHitsMurkup(data.hits);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 }
-// console.log('hi')
 
 function appendPhotoHitsMurkup(hits) {
   refs.gallery.insertAdjacentHTML('beforeend', createPhotoMarkup(hits));
+  let gallery = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+  });
+  gallery.refresh();
 }
 
 function createPhotoMarkup(searchQuery) {
@@ -54,7 +82,9 @@ function createPhotoMarkup(searchQuery) {
       comments,
       downloads,
     }) => `<div class="photo-card">
+    <a class='gallery__link' href='${largeImageURL}'>
   <img src="${webformatURL}" alt="${tags}" loading="lazy" width='400px' />
+  </a>
   <div class="info">
     <div>
       <p class="info-item">
@@ -86,4 +116,6 @@ function createPhotoMarkup(searchQuery) {
   );
 }
 
-console.log('hi')
+function clearGalleryMarkup() {
+  refs.gallery.innerHTML = '';
+}
